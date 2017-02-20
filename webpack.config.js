f75efdac9b26cc87cad7785421075a6390daf922
Kent Wills <rkwills@yelp.com>
2017-02-19 20:10:10 -0800

    update webpack config to be more optimal for production #19

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,55 @@
 var webpack = require("webpack");
 var path = require("path");
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
+
 const VENDOR = [
     'axios',
+    'lodash',
     'react',
     'react-router',
     'react-dom',
     'redux-form',
     'redux-promise'
 ];
+
+const plugins = [
+    new webpack.DefinePlugin({
+	'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    }),
+    new webpack.NamedModulesPlugin(),
+]
+
+if (isProd) {
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false
+      },
+    })
+  );
+} else {
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  );
+}
 
 module.exports = {
     entry: {
@@ -41,5 +82,8 @@ module.exports = {
     },
     resolve: {
 	extensions: ['.js', '.jsx'],
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 }
